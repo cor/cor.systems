f818008fc8ca3b5fec8556ed6995e5515bb4d052
cor <cor@pruijs.nl>
2018-03-17 17:36:54 +0100

    Add ultra basic site

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+@import url("https://fonts.googleapis.com/css?family=Bitter:400,400i,700");
+
+* {
+  color: #2c2c2c;
+}
+
+h1 {
+  font-size: 72px;
+  margin: 0;
+}
+
+h2 {
+  margin-top: -4px;
+  margin-bottom: 2px;
+}
+
+p {
+  font-size: 18px;
+  margin-top: 4px;
+}
+
+body {
+  margin: 0;
+  font-family: "Bitter", serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="info">
+      <h1>CorCoder</h1>
+      <h2>developer and designer</h2>
+      <p><a href="mailto:cor@pruijs.nl">cor@pruijs.nl</a></p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style lang="scss">
+.home {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
 
+<script>
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
