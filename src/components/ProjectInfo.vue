6698e09adedb43c33dabf5b2a4eb481b9c741484
cor <cor@pruijs.nl>
2019-01-23 21:55:53 +0100

    Add technology badges

--- src/components/ProjectInfo.vue
+++ src/components/ProjectInfo.vue
@@ -3,6 +3,7 @@
   <h3>{{ project.title }}</h3>
   <p class="project-date">{{ project.date }}</p>
   <p class="project-description">{{ project.description }} </p>
+  <div class="technology-badge" v-for="technology in project.technologies" :key="technology">{{technology}}</div>
   <p><a :href="projectLink">View on GitHub</a></p>
 </div>
 </template>
@@ -30,5 +31,18 @@ h3 {
 
 .project-description {
   margin-top: 0;
+}
+
+.technology-badge {
+  display: inline-block;
+  color: $white;
+  font-family: 'Teko', sans-serif;
+  background-color: $raspberry-pink;
+  padding: 2px 10px 0 10px;
+  margin: 0 4px;
+  border-radius: 4px;
+  &:first-of-type {
+    margin-left: 0;
+  }
 }
 </style>
