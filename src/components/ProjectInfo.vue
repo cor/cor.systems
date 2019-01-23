0a195649da58fb7a32541a3aa58f2f4cffc18acb
cor <cor@pruijs.nl>
2019-01-23 18:57:25 +0100

    Automate project info generation

---
+++ src/components/ProjectInfo.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="project-info">
+  <h3>{{ project.title }}</h3>
+  <p class="project-date">{{ project.date }}</p>
+  <p>{{ project.description }} </p>
+  <p><a :href="projectLink">View on GitHub</a></p>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  computed: {
+    projectLink () {
+      return `https://github.com/${this.project.githubPage}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  display: inline-block;
+}
+.project-date {
+  display: inline-block;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,7 @@
     <h2>Projects</h2>
     <p>This page is still in production, all other projects will be added shortly.</p>
 
-    <h3>swift-canvas</h3>
-    <p class="project-date">June 2014</p>
-    <p>A small drawing program written in Swift playgrounds that uses emoji to render simple geometric drawings.</p>
-    <p><a href="https://github.com/cor/swift-canvas">View on GitHub</a></p>
-
-    <h3>TicTacoToe</h3>
-    <p class="project-date">February 2014</p>
-    <p>This is my oldest project on GitHub, and thus the first project I made using a source control system. It's a console based Tic Tac Toe game written in Java.</p>
-    <p><a href="https://github.com/cor/tictacotoe">View on GitHub</a></p>
+    <project-info v-for="project in projects" :key="project.title" :project="project"/>
 
   </text-page>
 </template>
@@ -27,9 +19,16 @@ h3 {
 
 <script>
 import TextPage from '@/components/layout/TextPage.vue'
+import ProjectInfo from '@/components/ProjectInfo.vue'
+import projects from '@/projects.json'
 
 export default {
   name: 'About',
-  components: { TextPage }
+  components: { TextPage, ProjectInfo },
+  data () {
+    return {
+      projects: projects
+    }
+  }
 }
 </script>
