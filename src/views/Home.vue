812cb5e775320cf9338c4d784fd59ff42dce8079
cor <cor@pruijs.nl>
2019-01-26 14:11:24 +0100

    Fix 'to' typo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <text-page>
     <h2>About</h2>
     <p>I'm a 19 year old developer who is specialized in designing and building web applications using Vue.js, but likes doing (almost) every kind of programming.</p>
-    <p>My interest in programming started when I was 11 years old. My dad thaught me how write basic command line programs in C, and since then I've worked on a lot of different programming projects, using numerous languages, frameworks and technologies. I have worked professionally as a front end web developer and designer for a few clients, and continue to do so. I also occassionally deliver tech support to one person buisnesses, friends and my family.</p>
+    <p>My interest in programming started when I was 11 years old. My dad thaught me how to write basic command line programs in C, and since then I've worked on a lot of different programming projects, using numerous languages, frameworks and technologies. I have worked professionally as a front end web developer and designer for a few clients, and continue to do so. I also occassionally deliver tech support to one person buisnesses, friends and my family.</p>
     <p>While working on a lot of programming projects, I also managed to get my high school degree in 2018. I did VWO NT with extra Mathematics, Computer Science and Art as my chosen subjects.</p>
     <p>I plan on studying Computer Science and Game Development at the Utrecht University later this year.</p>
   </text-page>
