5a66983f14028a636a075fbcea5a08a3e05d0f88
cor <cor@pruijs.nl>
2019-01-26 13:15:39 +0100

    Finish projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,7 +2,6 @@
   <text-page>
     <h2>Projects</h2>
     <p>These are (almost) all projects I worked on. Some of them are personal, others were made for my study, and others are professional projects. The older the project, the worse the code is, but each project contributed to my programming experience and learning process.</p>
-    <p><i>This page is still under construction, and will be updated shortly.</i></p>
 
     <project-info v-for="project in projects" :key="project.title" :project="project"/>
 
