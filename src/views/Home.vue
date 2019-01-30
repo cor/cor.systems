b1ebe63e5a07479d866c6ea6b912e8c2c0e532a2
cor <cor@pruijs.nl>
2019-01-30 16:44:35 +0100

    Mention notable grades

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <h2>About</h2>
     <p>I'm a 19 year old developer who is specialised in designing and building web applications using Vue.js, but likes doing (almost) every kind of programming.</p>
     <p>My interest in programming started when I was 11 years old. My dad taught me how to write basic command line programs in C, and since then I've worked on a lot of different programming projects, using numerous languages, frameworks and technologies. I have worked professionally as a front end web developer and designer for a few clients, and continue to do so. I also occasionally deliver tech support to one person businesses, friends and my family.</p>
-    <p>While working on a lot of programming projects, I also managed to get my high school degree in 2018. I did VWO NT with extra Mathematics, Computer Science and Art as my chosen subjects.</p>
+    <p>While working on a lot of programming projects, I also managed to get my high school degree in 2018. I did VWO NT with extra Mathematics, Computer Science and Art as my chosen subjects. I graduated with a 9 for Mathematics B, Mathematics D and Computer Science, and an 8 for Physics and English.</p>
     <p>I plan on studying Computer Science and Game Development at the Utrecht University later this year.</p>
   </text-page>
 </template>
