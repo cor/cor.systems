9036f3efa4e3b4cd8107f02a35bebdb46b05c291
cor <cor@pruijs.nl>
2019-02-04 18:18:59 +0100

    Fix 'promblems' typo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <text-page>
     <h2>About</h2>
-    <p>I'm a 19 year old developer who is specialised in designing and building web applications using <i>Vue.js</i>, but likes doing (almost) every kind of programming. I enjoy working on creative solutions to complex promblems, learning about new technologies and working together with a great team.</p>
+    <p>I'm a 19 year old developer who is specialised in designing and building web applications using <i>Vue.js</i>, but likes doing (almost) every kind of programming. I enjoy working on creative solutions to complex problems, learning about new technologies and working together with a great team.</p>
     <p>My interest in programming started when I was 11 years old. My dad taught me how to write basic command line programs in C, and since then I've worked on a lot of different programming projects, using numerous languages, frameworks and technologies. I have worked professionally as a front end web developer and designer for multiple clients, and continue to do so. I also occasionally deliver tech support to one person businesses, friends and my family.</p>
     <p>While working on a lot of programming projects, I also managed to get my high school degree in 2018. I did VWO NT with extra <i>Mathematics, Computer Science</i> and <i>Art</i> as my chosen subjects. I graduated with a 9 for <i>Mathematics B, Mathematics D</i> and <i>Computer Science</i>, and an 8 for <i>Physics</i> and <i>English.</i></p>
     <p>I plan on studying <i>Computer Science and Game Development</i> at the Utrecht University later this year.</p>
