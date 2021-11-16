d57230e8ddbd0da2038189f86ea524637b604a12
cor <cor@pruijs.dev>
2021-11-16 13:23:21 +0100

    feat: purge template content

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import preprocess from 'svelte-preprocess';
+import { windi } from "svelte-windicss-preprocess";
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	preprocess: [
+		preprocess(),
+		windi({})
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
