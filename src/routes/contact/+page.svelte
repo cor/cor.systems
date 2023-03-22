82aeb5ebf96e482d2dbab2d4842c6490e3500269
cor <cor@pruijs.dev>
2023-03-22 15:22:03 +0100

    feat: add contact info

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,4 +5,14 @@
 
 <div class="text-column">
 	<h1>Contact</h1>
+	Feel free to contact me for business inquiries or casual chats!
+
+	<h2>Email</h2>
+	<a href="mailto:cor@pruijs.dev">cor@pruijs.dev</a>
+
+	<h2>Matrix</h2>
+	<a href="">corpruijs:matrix.org</a>
+
+	<h2>GitHub</h2>
+	<a href="https://github.com/cor">cor</a>
 </div>
